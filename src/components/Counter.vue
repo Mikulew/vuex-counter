9457276c1fd5d38a3e11f8e12a7acd1b9672775e
Mikulew <mikulew@gmail.com>
2021-07-19 20:44:33 +0200

    Add UI to Counter

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,11 +1,42 @@
 <template>
   <div>
     <h1>Vuex Counter</h1>
+    <div class="counter">0</div>
+    <button class="sign">-</button>
+    <input class="input" type="number" v-model="value" />
+    <button class="sign">+</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "Counter",
+  data() {
+    return {
+      counter: 0,
+      value: 1,
+    };
+  },
 };
 </script>
+
+<style scoped>
+.counter {
+  font-size: 5rem;
+}
+
+.sign {
+  border-radius: 100%;
+  border: none;
+  width: 2rem;
+  height: 2rem;
+  font-weight: 700;
+  cursor: pointer;
+}
+
+.input {
+  padding: 0.4rem;
+  margin: 0 0.5rem;
+  text-align: center;
+}
+</style>
