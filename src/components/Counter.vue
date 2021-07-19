5b39d8224888fc129251362eb18aa9984fb7ec98
Mikulew <mikulew@gmail.com>
2021-07-19 22:31:23 +0200

    Install axios and add actions

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,14 +2,19 @@
   <div>
     <h1>Vuex Counter</h1>
     <div class="counter">{{ counter }}</div>
-    <button class="sign" @click="substractFromCounter(parseInt(value))">-</button>
+    <button class="sign" @click="substractFromCounter(parseInt(value))">
+      -
+    </button>
     <input class="input" type="number" v-model="value" />
     <button class="sign" @click="addToCounter(parseInt(value))">+</button>
+    <div>
+      <button class="button" @click="addRandomNumber">Add Random Number</button>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex';
+import { mapState, mapMutations, mapActions } from "vuex";
 
 export default {
   name: "Counter",
@@ -19,11 +24,12 @@ export default {
     };
   },
   computed: {
-    ...mapState(["counter"])
+    ...mapState(["counter"]),
   },
   methods: {
-    ...mapMutations(["addToCounter", "substractFromCounter"])
-  }
+    ...mapMutations(["addToCounter", "substractFromCounter"]),
+    ...mapActions(["addRandomNumber"]),
+  },
 };
 </script>
 
@@ -45,5 +51,16 @@ export default {
   padding: 0.4rem;
   margin: 0 0.5rem;
   text-align: center;
+}
+
+.button {
+  border-radius: 0.5rem;
+  border: none;
+  width: auto;
+  background-color: #41b983;
+  margin-top: 1rem;
+  padding: 0.5rem;
+  color: #ffffff;
+  cursor: pointer;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createStore } from 'vuex'
 import App from './App.vue'
+import axios from 'axios'
 
 const store = createStore({
     state() {
@@ -14,6 +15,12 @@ const store = createStore({
         },
         substractFromCounter(state, payload) {
             state.counter = state.counter - payload;
+        }
+    },
+    actions: {
+        async addRandomNumber(context) {
+            let response = await axios.get("https://www.random.org/integers/?num=1&min=-1000&max=1000&col=1&base=10&format=plain&rnd=new");
+            context.commit("addToCounter", response.data)
         }
     }
 });
