aa5ba817565acaa5b78c548d9304bad68453d3ab
Mikulew <mikulew@gmail.com>
2021-07-19 22:54:17 +0200

    Add getters

--- src/components/Component3.vue
+++ src/components/Component3.vue
@@ -1,14 +1,19 @@
 <template>
-  <Component-4 />
+  <div>
+    <Component-4 />
+    <History />
+  </div>
 </template>
 
 <script>
 import Component4 from "./Component4.vue";
+import History from "./History.vue";
 
 export default {
   name: "Component3",
   components: {
     Component4,
+    History,
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -6,21 +6,35 @@ import axios from 'axios'
 const store = createStore({
     state() {
         return {
-            counter: 0
+            counter: 0,
+            history: [0]
         }
     },
     mutations: {
         addToCounter(state, payload) {
             state.counter = state.counter + payload;
+            state.history.push(state.counter);
         },
         substractFromCounter(state, payload) {
             state.counter = state.counter - payload;
+            state.history.push(state.counter);
         }
     },
     actions: {
         async addRandomNumber(context) {
             let response = await axios.get("https://www.random.org/integers/?num=1&min=-1000&max=1000&col=1&base=10&format=plain&rnd=new");
             context.commit("addToCounter", response.data)
+        }
+    },
+    getters: {
+        activeIndexes: (state) => (payload) => {
+            let indexes = [];
+            state.history.forEach((number, index) => {
+                if (number === payload) {
+                    indexes.push(index);
+                }
+            });
+            return indexes;
         }
     }
 });
